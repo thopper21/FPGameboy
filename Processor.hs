307b735b11513cff49a2127ac6e855b4f0b59970
Taylor <taylorhopper@outlook.com>
2016-12-30 14:58:27 -0800

    Added RegisterSet

--- Processor.hs
+++ Processor.hs
@@ -4,3 +4,17 @@ import Data.Int
 
 type EightBitRegister = Int8
 type SixteenBitRegister = Int16
+
+data RegisterSet = RegisterSet
+   {
+      a :: EightBitRegister,
+      b :: EightBitRegister,
+      c :: EightBitRegister,
+      d :: EightBitRegister,
+      e :: EightBitRegister,
+      h :: EightBitRegister,
+      l :: EightBitRegister,
+      f :: EightBitRegister,
+      pc :: SixteenBitRegister,
+      sp :: SixteenBitRegister
+   }
