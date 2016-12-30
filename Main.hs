60a1bb585657d625c5a7995baf1760810fd27f49
Taylor <taylorhopper@outlook.com>
2016-12-30 14:06:05 -0800

    Render functional pixels

--- Main.hs
+++ Main.hs
@@ -1,11 +1,54 @@
 module Main where
 
 import Graphics.UI.Gtk
-import Control.Monad.Trans(liftIO)
+import qualified Graphics.Rendering.Cairo as C
 
-main = do
-   initGUI
-   window <- windowNew
-   window `on` deleteEvent $ liftIO mainQuit >> return False
-   widgetShowAll window
-   mainGUI
+white = Color 65535 65535 65535
+
+defaultWidth = 300
+
+defaultHeight = 300
+
+main= do
+     initGUI
+     window <- windowNew
+     set window [windowTitle := "FPGameboy",
+                 windowDefaultWidth := 300, windowDefaultHeight := 300]
+
+     frame <- frameNew
+     containerAdd window frame
+     canvas <- drawingAreaNew
+     containerAdd frame canvas
+     widgetModifyBg canvas StateNormal white
+
+     widgetShowAll window 
+     drawin <- widgetGetDrawWindow canvas
+     onExpose canvas (handleExpose drawin)
+    
+     onDestroy window mainQuit
+     mainGUI
+
+handleExpose drawin x = do
+   renderWithDrawable drawin (render defaultWidth defaultHeight)
+   return False
+
+render width height =
+   let
+      renderFunc = uncurry $ renderPixel width height
+      pixels = [(x, y) | x <- [0..width], y <- [0..height]]
+   in
+      mapM renderFunc pixels
+  
+renderPixel width height x y = do
+   C.setSourceRGB r g b
+   C.rectangle x y 1 1
+   C.fill
+   where (r, g, b) = getPixel x y width height
+      
+
+getPixel x y width height =
+   let
+      red = x / width
+      green = y / height
+   in
+      (red, green, 0)
