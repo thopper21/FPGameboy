e206143858b02bb9dcdebcb64d1345849bca537a
Taylor <taylorhopper@outlook.com>
2016-12-30 17:38:55 -0800

    Memory map creation

--- Memory.hs
+++ Memory.hs
@@ -1,7 +1,8 @@
 module Memory where
 
-import Data.Int
 import Data.Array
+import Data.Bits
+import Data.Int
 
 type MemoryBank = Array Int16 Int8
 
@@ -11,7 +12,20 @@ data MemoryMap = MemoryMap
       videoRam :: MemoryBank,
       externalRam :: MemoryBank,
       workingRamBanks :: [MemoryBank],
-      spriteAttributes :: MemoryBank,
       highRam :: MemoryBank
    }
 
+kb size = shift 1 10
+
+createBank size = array (0, size - 1) [(i, 0) | i <- [0..size - 1]]
+
+createBanks count size = replicate count $ createBank size
+
+newMemoryMap romBankCount workingRamBankCount = MemoryMap
+   {
+      romBanks = createBanks romBankCount $ kb 16,
+      videoRam = createBank $ kb 8,
+      externalRam = createBank $ kb 8,
+      workingRamBanks = createBanks workingRamBankCount $ kb 4,
+      highRam = createBank 127
+   }
