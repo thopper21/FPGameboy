0c9cf707c34c1eda08f6001e4876f9bef506d542
Taylor <taylorhopper@outlook.com>
2016-12-30 17:22:06 -0800

    Defined the memory map

--- Memory.hs
+++ Memory.hs
@@ -1,1 +1,17 @@
 module Memory where
+
+import Data.Int
+import Data.Array
+
+type MemoryBank = Array Int16 Int8
+
+data MemoryMap = MemoryMap
+   {
+      romBanks :: [MemoryBank],
+      videoRam :: MemoryBank,
+      externalRam :: MemoryBank,
+      workingRamBanks :: [MemoryBank],
+      spriteAttributes :: MemoryBank,
+      highRam :: MemoryBank
+   }
+
